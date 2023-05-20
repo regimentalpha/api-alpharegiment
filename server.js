54b61b333f577678d71d0d675c9dee4148ce535a
regimentalpha <api.alpharegiment@gmail.com>
2023-05-20 13:36:11 +0530

    commit cors

--- server.js
+++ server.js
@@ -19,15 +19,7 @@ dotenv.config({ path: "./config/.env" });
 const app = express();
 
 // middlewares
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://senapariwar.com",
-      "https://alpharegiment.in",
-    ],
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
