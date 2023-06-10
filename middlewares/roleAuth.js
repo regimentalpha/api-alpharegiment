d31e6c540c97b56f0343e6ab0e7c92a5ace37986
regimentalpha <api.alpharegiment@gmail.com>
2023-06-10 12:40:23 +0530

    controller changes

--- middlewares/roleAuth.js
+++ middlewares/roleAuth.js
@@ -3,7 +3,7 @@ import ErrorHandler from "../utils/errorHandler.js";
 // User role authorization function
 const authorizeRoles = (...roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user?.role)) {
+    if (!roles.includes(req.user.role)) {
       return next(
         new ErrorHandler(
           `Role: ${req.user.role} is not allowed to access this resource`,
